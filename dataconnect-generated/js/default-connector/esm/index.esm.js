569bf9161158210bcee61ae675db17d3aa274d78
lionden90102 <141382750+lionden90102@users.noreply.github.com>
2025-02-06 21:44:08 +0000

    Update

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,8 @@
+import { , validateArgs } from 'firebase/data-connect';
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'simplelogin-registerwithfirebaseauthenticationusingjavascript-h',
+  location: 'us-central1'
+};
+
